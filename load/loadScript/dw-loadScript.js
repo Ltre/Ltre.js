69dc2ae2ab674fd860004af54a3da0d58483fedb
Ltre <ltrele@yeah.net>
2016-05-31 10:46:41 +0800

    a

--- load/loadScript/dw-loadScript.js
+++ load/loadScript/dw-loadScript.js
@@ -13,13 +13,13 @@ function loadScript(src){
                 o.onload = o.onreadystatechange = function(){
                     return o.readyState && "complete" !== o.readyState && "loaded" !== o.readyState ? !1 : (o.onload=o.onreadystatechange=null,void __load(count, src));
                 };
-                var c = new Date;
-                    c.setSeconds(0),
-                    c.setMilliseconds(0),
-                    c = c.getTime(),
-                    o.async = !0,
-                    o.src = n + "?_=" + c,
-                    document.getElementsByTagName("head")[0].appendChild(o);
+            var c = new Date;
+                c.setSeconds(0),
+                c.setMilliseconds(0),
+                c = c.getTime(),
+                o.async = !0,
+                o.src = n + "?_=" + c,
+                document.getElementsByTagName("head")[0].appendChild(o);
         }
     }(0, src);
 }
