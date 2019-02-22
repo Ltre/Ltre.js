7ce04013e0c1b4b7cce49f86b3429fb426f0aef2
Ltre <ltrele@yeah.net>
2019-02-22 20:45:21 +0800

    O0o-runtime

---
+++ data/crypt/O0o/O0o-guard.js
@@ -0,0 +1,28 @@
+function O0oGuard(code){
+    var c = '_$_$_("" ';
+    code.split('').forEach(function(e){
+        var n = e.charCodeAt();
+        c += '+$S5s(';
+        var na = [];
+        (n + '').split('').reverse().forEach(function(ee, i){
+            var nV = parseInt(ee) ? '_'+'$'.repeat(ee) : 0;//数字变量
+            var tV = Math.pow(10, i);//1, 10, 100, ...
+            console.log({e:e, ee:ee, nV:nV, tV:tV, c:c, n:n, na:na});
+            na.push(nV + '*' + tV);
+        });
+        c += na.join('+');
+        c += ')';
+    });
+    c += ');'
+    return c;
+}
+// 示例：
+// var code = 'alert(123);';
+// O0oGuard(code);
+/**
+
+生成代码：
+_$_$_("" +$S5s(_$$$$$$$*1+_$$$$$$$$$*10)+$S5s(_$$$$$$$$*1+0*10+_$*100)+$S5s(_$*1+0*10+_$*100)+$S5s(_$$$$*1+_$*10+_$*100)+$S5s(_$$$$$$*1+_$*10+_$*100)+$S5s(0*1+_$$$$*10)+$S5s(_$$$$$$$$$*1+_$$$$*10)+$S5s(0*1+_$$$$$*10)+$S5s(_$*1+_$$$$$*10)+$S5s(_$*1+_$$$$*10)+$S5s(_$$$$$$$$$*1+_$$$$$*10));
+
+ */
+// 执行后生成的代码，需要引入O0o-runtime.js才能运行
---
+++ data/crypt/O0o/O0o-runtime.js
@@ -0,0 +1,11 @@
+//基本支持，详见Ltre.js/data/crypt/O0o/O0o.js
+window.$S5s = String.fromCharCode;
+function _$$_(t){var $='';(t.replace(/o/g, [,].length))['split']('O').forEach(function(e){$+=$S5s(parseInt(e, [,,].length))});return $;}
+//变量支持
+!function($){
+    var $_ = window;
+    $<'\n'.charCodeAt() && ($_['_' + ('$'[_$$_('ooo00o0Ooo00o0oOooo0000Ooo00o0oOoo0000oOooo0o00')](++$))] = $) && arguments.callee.call(this, $);
+}(0);
+//运行时支持
+window._$_$_=window[_$$_("oo00o0oOooo0oo0Ooo0000oOoo0oo00")];
+
