308a839d42a8a61cb6124484c6c1c355674f0409
Ltre <ltrele@yeah.net>
2019-02-23 14:41:52 +0800

    fdsfsf

--- data/crypt/O0o/O0oGuard2.js
+++ data/crypt/O0o/O0oGuard2.js
@@ -1,5 +1,4 @@
 
-
 module.exports = function O0oGuard2(s){
     var $ = [];
     s.split('').forEach(function(e){
@@ -7,16 +6,3 @@ module.exports = function O0oGuard2(s){
     });
     return 'window._$_$_ = "' + $.join('O') + '";';
 }
-
-
-
-// 示例：
-// var code = 'alert(123);';
-// O0oGuard2(code);
-/**
-
-生成代码：
-
-
- */
-// 执行后生成的代码，需要引入O0o-runtime.js才能运行
--- data/crypt/O0o/test/O0o-guard-test2.js
+++ data/crypt/O0o/test/O0o-guard-test2.js
@@ -1,5 +1,6 @@
 // node O0o-guard-test.js
-
+// 生成的代码类似于 window._$_$_ = "oo0000oOoo0oo00Ooo00o0oOooo00o0Oooo0o00Oo0o000Ooo000oOoo00o0Ooo00ooOo0o00oOooo0oo";
+// 引入 O0o-runtime2.prod.js 后即可执行
 const fs = require('fs');
 const O0oGuard2 = require('../O0oGuard2');
 
