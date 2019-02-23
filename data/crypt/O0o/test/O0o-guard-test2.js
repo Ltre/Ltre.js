cb7ed86785eacd949eabc1c6be9a3f8617eae3e7
Ltre <ltrele@yeah.net>
2019-02-23 15:37:47 +0800

    codefile

--- data/crypt/O0o/test/O0o-guard-test2.js
+++ data/crypt/O0o/test/O0o-guard-test2.js
@@ -1,8 +1,9 @@
-// node O0o-guard-test.js
+// node O0o-guard-test.js --codefile=/path/to/jscode.js
 // 生成的代码类似于 window._$_$_ = "oo0000oOoo0oo00Ooo00o0oOooo00o0Oooo0o00Oo0o000Ooo000oOoo00o0Ooo00ooOo0o00oOooo0oo";
 // 引入 O0o-runtime2.prod.js 后即可执行
 const fs = require('fs');
 const O0oGuard2 = require('../O0oGuard2');
+const argv = require('optimist').argv;
 
-var code = fs.readFileSync('mycode.js', 'utf8');
+var code = fs.readFileSync(argv.codefile || 'mycode.js', 'utf8');
 console.log(O0oGuard2(code));
