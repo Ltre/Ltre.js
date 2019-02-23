7253ad59250e95f5278e47e31dd12a9026c491be
Ltre <ltrele@yeah.net>
2019-02-23 15:51:48 +0800

    如果线上执行错误，又npm install异常，则解压本目录的node_modules.zip

--- data/crypt/O0o/test/O0o-guard-test2.js
+++ data/crypt/O0o/test/O0o-guard-test2.js
@@ -1,6 +1,7 @@
 // node O0o-guard-test.js --codefile=/path/to/jscode.js
 // 生成的代码类似于 window._$_$_ = "oo0000oOoo0oo00Ooo00o0oOooo00o0Oooo0o00Oo0o000Ooo000oOoo00o0Ooo00ooOo0o00oOooo0oo";
 // 引入 O0o-runtime2.prod.js 后即可执行
+// 如果线上执行错误，又npm install异常，则解压本目录的node_modules.zip
 const fs = require('fs');
 const O0oGuard2 = require('../O0oGuard2');
 const argv = require('optimist').argv;
