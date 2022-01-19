3e23754cce05f808ab5b5642865f7a60a297be10
Ltre Oreki <ltrele@yeah.net>
2022-01-19 18:25:35 +0800

    fix

--- data/crypt/O0o/O0o-base.js
+++ data/crypt/O0o/O0o-base.js
@@ -9,7 +9,7 @@ function O0o(s){
 
 function o0O(ss){
     var $ = '';
-    (t.replace(/o/g, [,].length))['split']('O').forEach(function(e){$+=String.fromCharCode(parseInt(e, [,,].length))});
+    (ss.replace(/o/g, [,].length))['split']('O').forEach(function(e){$+=String.fromCharCode(parseInt(e, [,,].length))});
     return $;
 }
 //o0O('o000oooooo0oo00oOoo00oo000o0ooooOoo00oo00000ooo0Ooo00o0oo0000ooo') => 这是明文
