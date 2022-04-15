4939c9ffa390239304b13a78c12a52c44650f16d
Ltre Oreki <ltrele@yeah.net>
2022-04-15 15:43:38 +0800

    规范化使用uu

--- net/universalUpload/universalUpload.js
+++ net/universalUpload/universalUpload.js
@@ -1,31 +1,73 @@
 window.Ltrelib = window.Ltrelib || {};//使用一个生僻的名称作为全局变量，以存储自定义的库，防止与其它变量冲突
 
-//支持多图、多类型上传，能够多样化处理上传结果
-//上传的最基本控件：input:file
-//核心过程：
-//  生成隐藏的input:file控件；
-//  监听input:file的change事件
-//  通过任何方式，触发input:file被click，注意触发前需要清理input:file上绑定的数据
-//  当change事件发生后，有以下过程可参数化：
-//      0、[onNoFile]没有选择文件时，可能需要的额外处理过程
-//      1、[onBefore]上传前，这时已确定有选择文件，可以用来判断已选文件个数、文件属性分析等操作；如果onBefore返回false，则调用onEnd清理现场
-//      2、[onStart]上传开始时，一般隐藏结果视图、显示加载中视图
-//      3、[onUploading]上传中，一般用来渲染进度百分比
-//      4、[onWaiting]上传完成，等待服务端处理和响应。一般此时会作类似"100%, 处理中.."的提示
-//      5、[onResponse]服务端成功响应时执行, 执行完毕后会接着调用onEnd
-//      6、[onNetError]通信错误时执行, 执行完毕后会接着调用onEnd
-//      7、[onEnd]全部流程完成或被中断时，一般用于清理现场
-//设置的条件：
-//  1、[api]设置上传地址
-//  2、[fileInputName]设置FormData文件域的名称，例如filedata，核心代码将实现为 var fd = new FormData(); fd.append('filedata', file或files)
-//  3、[bizParams]设置其它参数表，根据具体业务
-//  4、[isMultiple]是否多文件上传
-//  5、[uploadWay]上传途径：raw|jquery
-//  6、[respType]响应类型：默认使用json响应, 全部值：xml,html,script,json,text。
-//               因上传不适用GET，故不含jsonp。
-//               在jQuery上传模式下，支持全部值；
-//               在原生AJAX上传模式下，仅支持json和text
-Ltrelib.universalUpload = function(){
+/**
+ * 支持多图、多类型上传，能够多样化处理上传结果
+ * 
+ * 核心过程：
+ *      1、生成隐藏的input:file控件；
+ *      2、监听input:file的change事件；
+ *      3、通过任何方式，触发input:file被click；
+ *      4、在上传的每个过程执行自定义逻辑；
+ *      5、使用FormData和XHR上传文件
+ * 
+ * 当input:file的change事件发生后，以下过程可参数化：
+ *      0、[onNoFile]没有选择文件时，可能需要的额外处理过程
+ *      1、[onBefore]上传前，这时已确定有选择文件，可以用来判断已选文件个数、文件属性分析等操作；如果onBefore返回false，则调用onEnd清理现场
+ *      2、[onStart]上传开始时，一般隐藏结果视图、显示加载中视图
+ *      3、[onUploading]上传中，一般用来渲染进度百分比
+ *      4、[onWaiting]上传完成，等待服务端处理和响应。一般此时会作类似"100%, 处理中.."的提示
+ *      5、[onResponse]服务端成功响应时执行, 执行完毕后会接着调用onEnd
+ *      6、[onNetError]通信错误时执行, 执行完毕后会接着调用onEnd
+ *      7、[onEnd]全部流程完成或被中断时，一般用于清理现场
+ * 
+ * 基本配置条件：
+ *      1、[api]设置上传地址
+ *      2、[fileInputName]设置FormData文件域的名称，例如filedata，核心代码将实现为 var fd = new FormData(); fd.append('filedata', file或files)
+ *      3、[bizParams]设置其它参数表，根据具体业务
+ *      4、[isMultiple]是否多文件上传
+ *      5、[uploadWay]上传途径：raw|jquery
+ *      6、[respType]响应类型：默认使用json响应, 全部值：xml,html,script,json,text。
+ *          因上传不适用GET，故不含jsonp。
+ *          在jQuery上传模式下，支持全部值；
+ *          在原生AJAX上传模式下，仅支持json和text
+ * 
+ * 示例1 (标记过时, 不推荐)：
+ *      let uu = new Ltrelib.universalUpload;
+ *      uu.api = 'xxxxx';
+ *      uu.fileInputName = 'yyyyy';
+ *      ...
+ *      uu.bindTrigger(function(trigger){
+ *          $('#example').click(trigger);
+ *      });
+ * 
+ * 示例2：
+ *      let uu = new Ltrelib.universalUpload;
+ *      uu.api = 'xxxxx';
+ *      uu.fileInputName = 'yyyyy';
+ *      //代替bindTrigger()
+ *      uu.trigger = {domSelector:'#example', evtType:'click'};
+ *      ...
+ * 
+ * 示例3:
+ *      let uu = new Ltrelib.universalUpload;
+ *      uu.setup({
+ *          api: 'xxxx',
+ *          fileInputName = 'yyyy',
+ *          trigger: {domSelector:'#example', evtType:'click'},
+ *          ...
+ *      });
+ * 
+ * 示例4:
+ *      let uu = new Ltrelib.universalUpload({
+ *          api: 'xxxx',
+ *          fileInputName = 'yyyy',
+ *          trigger: {domSelector:'#example', evtType:'click'},
+ *          ...
+ *      });
+ *      
+ * @param {Object} setupArgs 一次性配置所有参数（可选）
+ */
+Ltrelib.universalUpload = function(setupArgs){
 
     var that = this;
     var raw = {};
@@ -39,6 +81,7 @@ Ltrelib.universalUpload = function(){
     that.acceptMime = '';
     that.uploadWay = 'raw';
     that.respType = 'json';
+    that.trigger = {domSelector: '#example', evtType: 'click'};
     
     that.onNoFile = function(){};
     that.onBefore = function(files){};
@@ -52,7 +95,8 @@ Ltrelib.universalUpload = function(){
     setInterval(function(){
         let ifThenMap = {
             isMultiple: buildFileInput,//监听并重载上传控件
-            acceptMime: buildFileInput//监听并重载上传控件
+            acceptMime: buildFileInput,//监听并重载上传控件
+            trigger: bindTrigger,
         };
         for (k in ifThenMap){
             if (!(k in raw) || raw[k] !== that[k]) {
@@ -166,7 +210,7 @@ Ltrelib.universalUpload = function(){
         xhr.withCredentials = true;
         xhr.send(formdata);
     };
-    
+
     var onChangeFileInput = function(evt){
         var files = evt.currentTarget.files;
         if (! (files.length > 0)) {
@@ -185,7 +229,7 @@ Ltrelib.universalUpload = function(){
             upload(fd);
         }
     };
-    
+
     var buildFileInput = function(){
         if (! fileInputId) {
             fileInputId = 'fuckFile' + (+ new Date()) + Math.floor(Math.random()*100000);
@@ -202,25 +246,24 @@ Ltrelib.universalUpload = function(){
             $('#' + fileInputId).attr(attrs);
         }
     };
-    
-    //批量设置
-    that.setup = function(map){
-        if ('object' != typeof map) throw new Error('map参数有误');
-        var keys = [
-            'api', 'fileInputName', 'bizParams', 'isMultiple', 'uploadWay', 'respType',
-            'onNoFile', 'onBefore', 'onStart', 'onUploading', 'onWaiting', 'onResponse', 'onEnd'
-        ];
-        for (var i in keys) {
-            if (keys[i] in map) {
-                that[keys[i]] = map[keys[i]];
-            }
-        }
-    };
-    
-    //绑定触发上传的过程，例如点击某按钮、拖放文件等
-    that.bindTrigger = function(callback){
-        checkSetup();
-        callback.call(this, function(evt){
+
+    /**
+     * 绑定触发源及触发方式
+     *      例如点击某按钮、拖放文件等 (该方法只能执行一次).
+     *      触发前会清理input:file上已有的文件数据.
+     * 
+     * @date 20220415调整：
+     *      不再推荐直接使用此接口，
+     *      而是通过设置trigger属性，以完成绑定。
+     * 
+     * @param {Function} callback 当本方法作为外部接口提供时，所需传入的具体事件绑定方法，其接受一个用于事件回调的Function型参数
+     */
+    var bindTrigger = function(callback){
+        if (this.__lockOfBindTrigger) return;
+        this.__lockOfBindTrigger = true;
+
+        // checkSetup();
+        let thencb = function(evt){
             if(!! document.all) {
                 fileInput[0].select();
                 document.execCommand("delete");
@@ -228,8 +271,43 @@ Ltrelib.universalUpload = function(){
                 fileInput[0].value = '';
             }
             fileInput.click();
-        });
+        };
+
+        if (callback && typeof callback == 'function') {
+            callback.call(this, thencb);
+        } else {
+            $('body').on(that.trigger.evtType, that.trigger.domSelector, thencb);
+        }
     };
 
+    //批量设置
+    var setup = function(map){
+        if ('object' != typeof map) throw new Error('参数错误');
+        var keys = [
+            'api', 'fileInputName', 'bizParams', 'isMultiple', 'acceptMime', 'uploadWay', 'respType', 'trigger',
+            'onNoFile', 'onBefore', 'onStart', 'onUploading', 'onWaiting', 'onResponse', 'onEnd'
+        ];
+        for (var i in keys) {
+            if (keys[i] in map) {
+                that[keys[i]] = map[keys[i]];
+            }
+        }
+        checkSetup();
+    };
+    
+    //根据构造参数，决定是否立即初始化配置
+    if ('object' == typeof setupArgs) { //立即进行
+        setup(setupArgs);
+        that.setup = function(){
+            throw new Error('已设置过初始化参数，禁止再次设置');
+        };
+    } else { //延后进行
+        that.setup = setup;
+    }
+
+    //开放触发过程的绑定接口（这个是旧用法，新用法建议使用trigger属性）
+    that.bindTrigger = bindTrigger;
+
+    //在初始化配置完成后，立即生成文件控件
     buildFileInput();
 };
