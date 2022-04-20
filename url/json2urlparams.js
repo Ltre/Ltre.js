98c0950d0d3801441fde30555f7234043fe6460a
Ltre Oreki <ltrele@yeah.net>
2022-04-20 14:12:45 +0800

    Update json2urlparams.js

--- url/json2urlparams.js
+++ url/json2urlparams.js
@@ -1,15 +1,12 @@
-//示例
 
-  $("#query").click(function(){
-    let url = "<{url r='stat/flowrecord' page=$pages type=$type}>";
-    let params = {
-      startTime: $('#startTime').val(),
-      endTime: $('#endTime').val(),
-      kw: $('#kw').val()
-    };
+let url = '/xxxxx';
+let params = {
+  a: 1,
+  b: 2,
+  c: 3
+};
+
+location.href = url + (/\?/.test(url) ? '&' : '?') + Object.keys(params).map(function(k){
+  return encodeURIComponent(k) + '=' + encodeURIComponent(params[k]);
+}).join('&');
     
-    location.href = url + (/\?/.test(url) ? '&' : '?') + Object.keys(params).map(function(k){
-      return encodeURIComponent(k) + '=' + encodeURIComponent(params[k]);
-    }).join('&');
-    
-  });
